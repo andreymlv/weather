d918baf4ae88af52734f9de30c6029b2da431538
andreymlv <ndrmlv@yandex.ru>
2022-01-26 23:24:47 +0300

    added lib

---
+++ lib/Screen.cs
@@ -0,0 +1,16 @@
+﻿namespace lib;
+
+public class Screen
+{
+    private readonly string _message;
+
+    public Screen(string message)
+    {
+        _message = message;
+    }
+
+    public override string ToString()
+    {
+        return _message;
+    }
+}
--- weather/Program.cs
+++ weather/Program.cs
@@ -1,9 +1,12 @@
 ﻿namespace weather;
 
+using lib;
+
 internal static class Program
 {
     private static void Main()
     {
         Console.WriteLine("Hello World!");
+        Console.WriteLine(new Screen("Test screen"));
     }
 }
